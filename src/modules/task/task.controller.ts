bcb907d1333a17071b06eafdb38925cb0ac62605
elmanee <cp457920@gmail.com>
2026-02-09 10:30:00 -0600

    feat: implementacion de controladores y servicios para el modulo de tareas

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('/api/task')
 export class TaskController {
   constructor(private readonly taskSvc: TaskService) {}
-
-  @Get('/login')
-  public login(): string {
-    return this.taskSvc.login();
+  // ? http://localhost:3000/api/task
+  @Get()
+  public getTask(): any {
+    return this.taskSvc.getTasks();
+  }
+  //!GET  http://localhost:3000/api/task/1
+  @Get(':id')
+  public getTaskById(@Param('id') id: string): any {
+    return this.taskSvc.getTaskById(parseInt(id));
+  }
+  //* POST http://localhost:3000/api/task
+  @Post()
+  public insertTask(task: any): any {
+    return this.taskSvc.insertTask(task);
+  }
+  //! PUT http://localhost:3000/api/task/:id
+  @Put(':id')
+  public updateTask(id: number, task: any): any {
+    return this.taskSvc.updateTask(id, task);
+  }
+  //? DELETE http://localhost:3000/api/task/:id
+  @Delete(':id')
+  public deleteTask(id: number): string {
+    return this.taskSvc.deleteTask(id);
   }
 }
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -2,7 +2,20 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TaskService {
-  public login(): string {
-    return 'Task exitoso';
+  public getTasks(): string {
+    return 'Lista de tareas';
+  }
+  public getTaskById(id: number): string {
+    return `Se obtiene la tarea con id ${id}`;
+  }
+  public insertTask(task: any): any {
+    return task;
+  }
+  public updateTask(id: number, task: any): any {
+    return task;
+  }
+
+  public deleteTask(id: number): string {
+    return `Se elimina la tarea con id ${id}`;
   }
 }
